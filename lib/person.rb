6234cbb09d3a02c2e09cb3b792f02fe7ab945a95
Shakeria Smith <shakerias2@gmail.com>
2018-07-17 18:00:57 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,10 @@ adele_goldberg = Person.new
 alan_kay = Person.new
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> 12a64989a5d8a466c26b6797c0591ba818697c7a
 =======
 
 >>>>>>> 12a64989a5d8a466c26b6797c0591ba818697c7a
